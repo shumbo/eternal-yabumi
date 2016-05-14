58beef94cd233cf40974d2990a77e66780c5d574
Shun Kashiwa <shun.kashiwa@live.jp>
2016-05-14 19:11:40 +0900

    First commit

---
+++ setter.js
@@ -0,0 +1,7 @@
+  setTimeout(function(){
+    console.log("fire");
+    var e = document.querySelectorAll(".flagrate-button");
+    e[2].click();
+    e = document.querySelectorAll(".flagrate-button");
+    e[8].click();
+  },500);
